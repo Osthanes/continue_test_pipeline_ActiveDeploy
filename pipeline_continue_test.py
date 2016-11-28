462e096216182c15c53a90516882f67f529f7f42
BHeubach <bheubach@de.ibm.com>
2016-11-28 15:01:10 +0100

    issue 231 wi: https://github.ibm.com/ActiveDeploy/team/issues/231

--- pipeline_continue_test.py
+++ pipeline_continue_test.py
@@ -119,6 +119,10 @@ def main():
         print "\nSuccessfully retrieved pipeline information AFTER test run ..."
         for item in curr_pipe_info:
             print item[0], ', '.join(map(str, item[1:]))
+            print "\n"
+            #check for any FAILURE in item
+            if 'FAILURE' in item:
+                raise Exception("\nThe test run was not run successfully.")
     else:
         raise Exception("\nThe test run was not run successfully.")
 
